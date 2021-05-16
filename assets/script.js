55c3a777c2b69d3a2caf47d08efa0fd1b9b19ab5
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-05-15 19:40:20 -0500

    decided on time format, removed 6-8am and 6pm

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 function clockRefresh() {
     let clock = document.getElementById("currentDay");
     let DateTime = luxon.DateTime;
-    clock.textContent = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+    clock.textContent = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
 }
 
 let time = setInterval(clockRefresh, 1000);
